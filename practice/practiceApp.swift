633a8db1f6e76983e6cf7ee6a3969865b7e846cb
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-09 18:37:42 +0900

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Takuya Ando on 2020/11/09.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
